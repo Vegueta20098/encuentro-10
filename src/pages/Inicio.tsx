e78d74a6dc3ca9cb3dd1ede57c529d05283bcb48
Vegueta20098 <est.orosalesr@uml.edu.ni>
2023-08-05 12:35:38 -0600

    api

--- src/pages/Inicio.tsx
+++ src/pages/Inicio.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from 'react';
 import { Title } from '../components/Title';
 
 export const Inicio = () => {
+
+  useEffect ( () => {
+    fetch('https://jsonplaceholder.typicode.com/posts/2')
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+  }, []);
+
   return (
     <>
       <Title titulo="Bienvenido!" />
